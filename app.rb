758600aec5c4a5af0c8e5ba76b02ce510f2f0107
Jessa Marie Barre <jmlbarre@gmail.com>
2017-07-26 10:53:02 -0400

    moved pokemon routes and created home link in layout

--- app.rb
+++ app.rb
@@ -3,48 +3,13 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 require_relative 'db/connection'
+
 require_relative 'models/pokemon'
+require_relative 'models/trainer'
 
+require_relative 'routes/pokemon'
+require_relative 'routes/trainers'
 
-# READ all pokemon
-get '/pokemon' do
-  @pokemon = Pokemon.all
-  erb :'pokemon/index'
-end
-
-# READ create pokemon form from user
-get '/pokemon/new' do
-  erb :'pokemon/new'
-end
-
-# CREATE new pokemon
-post '/pokemon' do
-  @poke = Pokemon.create(params[:poke])
-  redirect "/pokemon/#{@poke.id}"
-end
-
-# READ one pokemon
-get '/pokemon/:id' do
-  @poke = Pokemon.find(params[:id])
-  erb :'pokemon/show'
-end
-
-# READ update pokemon form from user
-get '/pokemon/:id/edit' do
-  @poke = Pokemon.find(params[:id])
-  erb :'pokemon/edit'
-end
-
-# UPDATE pokemon
-put '/pokemon/:id' do
-  @poke = Pokemon.find(params[:id])
-  @poke.update(params[:poke])
-  redirect "/pokemon/#{@poke.id}"
-end
-
-# DELETE pokemon by id
-delete '/pokemon/:id' do
-  @poke = Pokemon.find(params[:id])
-  @poke.destroy
-  redirect '/pokemon'
+get '/' do
+  erb :index
 end
---
+++ routes/pokemon.rb
@@ -0,0 +1,42 @@
+# READ all pokemon
+get '/pokemon' do
+  @pokemon = Pokemon.all
+  erb :'pokemon/index'
+end
+
+# READ create pokemon form from user
+get '/pokemon/new' do
+  erb :'pokemon/new'
+end
+
+# CREATE new pokemon
+post '/pokemon' do
+  @poke = Pokemon.create(params[:poke])
+  redirect "/pokemon/#{@poke.id}"
+end
+
+# READ one pokemon
+get '/pokemon/:id' do
+  @poke = Pokemon.find(params[:id])
+  erb :'pokemon/show'
+end
+
+# READ update pokemon form from user
+get '/pokemon/:id/edit' do
+  @poke = Pokemon.find(params[:id])
+  erb :'pokemon/edit'
+end
+
+# UPDATE pokemon
+put '/pokemon/:id' do
+  @poke = Pokemon.find(params[:id])
+  @poke.update(params[:poke])
+  redirect "/pokemon/#{@poke.id}"
+end
+
+# DELETE pokemon by id
+delete '/pokemon/:id' do
+  @poke = Pokemon.find(params[:id])
+  @poke.destroy
+  redirect '/pokemon'
+end
